ceda2f75d1bc1b27dd94d8295dafb63332b333c5
Santiago Mesa Parra <86623637+SantiagoMesa0911@users.noreply.github.com>
2023-10-03 19:26:45 -0500

    GifsService

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -13,11 +14,15 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   tagInput!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor(private gifsService: GifsService) { }
 
 
   searchTag() {
     const newTag = this.tagInput.nativeElement.value
+
+    this.gifsService.searchTags(newTag);
+
+    this.tagInput.nativeElement.value = ''
   }
 
 }
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _tagsHistory: string[] = [];
+
+
+
+  constructor() { }
+
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistoryTags(tag: string) {
+    tag = tag.toLowerCase()
+    if (this.tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag)
+    }
+
+    this._tagsHistory.unshift(tag)
+
+    this._tagsHistory = this._tagsHistory.slice(0, 10)
+
+  }
+
+  searchTags(tag: string): void {
+    if (tag.length === 0) return
+    this.organizeHistoryTags(tag)
+  }
+}
